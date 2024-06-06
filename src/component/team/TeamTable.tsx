f5f7cc4135989cc07bb10b573c49aa93dd3c0f97
taeyoung kang <kty1467@naver.com>
2024-06-06 16:55:51 +0900

    팀 매칭 목록화 완료

--- src/component/team/TeamTable.tsx
+++ src/component/team/TeamTable.tsx
@@ -1,9 +1,105 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import styles from "../../css/team/TeamTable.module.css";
+import { Link } from 'react-router-dom';
+import { skillData } from '../../data/skillData';
 
-const TeamTable = () => {
+interface TeamProps {
+    teamData: {
+        dtoList: {
+            idx: number;
+            ttitle: string;
+            tnickname: string;
+            tskill: string;
+            state: number;
+            regDate: string;
+            replyCount: number;
+        } [];
+        totalPage: number;
+        page: number;
+        prev: boolean;
+        next: boolean;
+        pageList: number[];
+    };
+    currentPage: number; // 추가: 현재 페이지 상태
+    onPageClick: (pageNum: number) => void;
+}
+
+
+const TeamTable: React.FC<TeamProps> = ({teamData, currentPage, onPageClick}) => {
+    // 전체, 모집중, 모집완료 선택
+    const [selected, setSelected] = useState('전체');
+    const handleClick = (text: string) => {
+        setSelected(text);
+    };
+
+    // tskill 값을 처리하는 함수
+    const getSkillLabels = (tskill: string) => {
+        const skillIds = tskill.split(',').map(Number); // 쉼표로 분리하고 정수로 변환
+        const skillLabels = skillIds.map(id => {
+            const skill = skillData.find(skill => skill.value === id);
+            return skill ? skill.label : '';
+        });
+    
+        // 4개까지만 표시하고 나머지는 ...으로 표시
+        if (skillLabels.length > 4) {
+            return skillLabels.slice(0, 4).join(', ') + ' ...';
+        } else {
+            return skillLabels.join(', '); // 문자열로 합침
+        }
+    };
+
     return(
         <div className={styles.tableBox}>
-
+            <div className={styles.buttonTable}>
+                {['전체', '모집중', '모집완료'].map((text) => (
+                    <span
+                        key={text}
+                        className={`${styles.text} ${selected === text ? styles.selected : ''}`}
+                        onClick={() => handleClick(text)}
+                    >
+                        {text}
+                    </span>
+                ))}
+                <button className={`${styles.registerButton} ${styles.comment}`}>글 작성</button>
+            </div>
+            <div className={styles.dataTable}>
+                {teamData.dtoList
+                    .filter(item => {
+                        if (selected === '모집중') {
+                            return item.state === 0;
+                        } else if (selected === '모집완료') {
+                            return item.state === 1;
+                        }
+                        return true; // '전체'가 선택된 경우 모든 항목을 반환
+                    })
+                    .map((item) => (
+                        <div key={item.idx} className={styles.box}>
+                            <div className={styles.header}>
+                                <text className={styles.status}>{item.state === 0 ? '모집중' : '모집완료'}</text>
+                                <text className={styles.itemTitle}>{item.ttitle}</text>
+                            </div>
+                            <div className={styles.body}>
+                                <text className={styles.itemText}>작성자 : {item.tnickname}</text>
+                                <text className={styles.itemText}>작성일 : {item.regDate}</text>
+                                <text className={styles.itemText}>사용기술 : {getSkillLabels(item.tskill)}</text>
+                                <text className={`${styles.itemText} ${styles.comment}`}>{item.replyCount}</text>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+            <div className={styles.pagination}>
+                {teamData.pageList.map((pageNum) => (
+                    <button
+                        key={pageNum}
+                        className={pageNum === currentPage ? 'active' : ''}
+                        onClick={() => onPageClick(pageNum)}
+                    >
+                        {pageNum}
+                    </button>
+                ))}
+            </div>
         </div>
     );
 }
--- src/pages/TeamMatePage.tsx
+++ src/pages/TeamMatePage.tsx
@@ -1,15 +1,56 @@
 import Header from "../component/common/Header";
 import Menu from "../component/common/Menu";
-import TeamBox from "../component/team/TeamBox";
 import styles from "../css/team/Team.module.css";
+import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import TeamMenu from "../component/team/TeamMenu";
+import TeamTable from "../component/team/TeamTable";
 
 
 const TeamMatePage = () => {
+    const [teamData, setTeamData] = useState({
+        dtoList: [],
+        totalPage: 0,
+        page: 0,
+        prev: false,
+        next: false,
+        pageList: [],
+    })
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [searchValue, setSearchValue] = useState('');
+    const [ktypeValue, setKtypeValue] = useState('');
+
+    const PageClick = (pageNum: number) => {
+        setCurrentPage(pageNum);
+    };
+
+    const fetchData = async (searchValue = '', ktypeValue = '') => {
+        try {
+            const params = new URLSearchParams();
+            if (ktypeValue) params.append('ktype', ktypeValue);
+            if (searchValue) params.append('search', searchValue);
+            const url = `http://localhost:6600/team/list?page=${currentPage}${params.toString()}`;
+            const response = await axios.get(url);
+            setTeamData(response.data);
+            console.log(response.data);
+        } catch (error) {
+            console.error('게시판 데이터를 불러오는 도중 에러 발생:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchData(searchValue, ktypeValue);
+    }, [currentPage, searchValue, ktypeValue]);
+
     return(
         <div className={styles.teamBox}>
             <Header/>
             <Menu/>
-            <TeamBox/>
+            <div className={styles.teamBox2}>
+                <TeamMenu/>
+                <TeamTable teamData={teamData} currentPage={currentPage} onPageClick={PageClick}/>
+            </div>
         </div>
     );
 }
